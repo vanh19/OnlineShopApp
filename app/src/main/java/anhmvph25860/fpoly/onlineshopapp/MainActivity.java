fd9a0b1b8cc4a8c6448786bd3e684d85b17ef2c2
vanh19 <anhmv19@gmail.com>
2023-12-26 05:30:21 +0700

    MainUI

---
+++ app/src/main/java/anhmvph25860/fpoly/onlineshopapp/MainActivity.java
@@ -0,0 +1,14 @@
+package anhmvph25860.fpoly.onlineshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
